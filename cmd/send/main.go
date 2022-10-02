bab2bd19e3f7655f0ff0e7281fc38670655bbff0
Fernando Fabricio dos Santos <ferfabricio@gmail.com>
2022-10-01 23:25:00 -0300

    refactor

--- cmd/send/main.go
+++ cmd/send/main.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"encoding/base64"
-	"fmt"
 	"os"
 
+	"github.com/ferfabricio/certificados-devparana-go/internal/data"
+	"github.com/ferfabricio/certificados-devparana-go/internal/email"
 	"github.com/joho/godotenv"
-	"github.com/sendgrid/sendgrid-go"
-	"github.com/sendgrid/sendgrid-go/helpers/mail"
 )
 
 func main() {
@@ -16,45 +14,22 @@ func main() {
 		panic("Error loading .env file")
 	}
 
-	m := mail.NewV3Mail()
+	a := os.Args
+	if len(a) == 1 {
+		panic("missing required argument")
+	}
 
-	from := mail.NewEmail("DevParaná", "contato@devparana.org")
-	to := mail.NewEmail("Everton Tavares", "ezidiu@gmail.com")
-
-	m.SetFrom(from)
-	m.SetTemplateID(os.Getenv("SENDGRID_CERTIFICATE_TEMPLATE"))
-
-	p := mail.NewPersonalization()
-	p.AddTos(to)
-	p.SetDynamicTemplateData("first_name", "Everton Tavares")
-
-	m.AddPersonalizations(p)
-
-	a := mail.NewAttachment()
-	c, err := os.ReadFile("./output/83064af3-bb81-4514-a6d4-afba340825cd.pdf")
+	c, err := data.GetCollection(a[1])
 	if err != nil {
 		panic(err)
 	}
-	encoded := base64.StdEncoding.EncodeToString([]byte(c))
 
-	a.SetContent(encoded)
-	a.SetType("application/pdf")
-	a.SetFilename("certificado.pdf")
-	a.SetDisposition("attachment")
-
-	m.AddAttachment(a)
-
-	request := sendgrid.GetRequest(os.Getenv("SENDGRID_API_KEY"), "/v3/mail/send", "https://api.sendgrid.com")
-	request.Method = "POST"
-	var Body = mail.GetRequestBody(m)
-	request.Body = Body
-	response, err := sendgrid.API(request)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println(response.StatusCode)
-		fmt.Println(response.Body)
-		fmt.Println(response.Headers)
+	for i := 0; i < len(c.Cities); i++ {
+		for j := 0; j < len(c.Cities[i].Attendants); j++ {
+			err = email.Send(&c.Cities[i].Attendants[j])
+			if err != nil {
+				panic(err)
+			}
+		}
 	}
-
 }
---
+++ internal/email/sender.go
@@ -0,0 +1,55 @@
+package email
+
+import (
+	"encoding/base64"
+	"fmt"
+	"os"
+
+	"github.com/ferfabricio/certificados-devparana-go/internal/data"
+	"github.com/sendgrid/sendgrid-go"
+	"github.com/sendgrid/sendgrid-go/helpers/mail"
+)
+
+func Send(a *data.Attendant) error {
+
+	m := mail.NewV3Mail()
+
+	from := mail.NewEmail("DevParaná", "contato@devparana.org")
+	to := mail.NewEmail(a.Name, a.Email)
+
+	m.SetFrom(from)
+	m.SetTemplateID(os.Getenv("SENDGRID_CERTIFICATE_TEMPLATE"))
+
+	p := mail.NewPersonalization()
+	p.AddTos(to)
+	p.SetDynamicTemplateData("first_name", a.Name)
+
+	m.AddPersonalizations(p)
+
+	at := mail.NewAttachment()
+	c, err := os.ReadFile(fmt.Sprintf("./output/%s.pdf", a.Code))
+	if err != nil {
+		return err
+	}
+	encoded := base64.StdEncoding.EncodeToString([]byte(c))
+
+	at.SetContent(encoded)
+	at.SetType("application/pdf")
+	at.SetFilename("certificado.pdf")
+	at.SetDisposition("attachment")
+
+	m.AddAttachment(at)
+
+	request := sendgrid.GetRequest(os.Getenv("SENDGRID_API_KEY"), "/v3/mail/send", "https://api.sendgrid.com")
+	request.Method = "POST"
+	var Body = mail.GetRequestBody(m)
+	request.Body = Body
+	response, err := sendgrid.API(request)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Email enviado para %s, Status Code: %d\n", a.Email, response.StatusCode)
+
+	return nil
+}
